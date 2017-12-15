5d6aa4e568203468a4681f8da58c69043315e986
LinJoseph <lyz2356002@gmail.com>
2017-12-15 19:22:51 +0800

    add internet connection

--- water_data.py
+++ water_data.py
@@ -1,24 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import requests
 import json
 
-def getData():
-    url="http://123.127.175.45:8082/ajax/GwtWaterHandler.ashx"
-    data = {"Method":"SelectRealData"}
+
+def get_connect():
+    url = "https://lgn.bjut.edu.cn"
+    data = {"DDDDD": "S201761811", "upass": "2356002", "v46s": "1", "v6ip": "", "f4serip": "172.30.201.10",
+            "0MKKey": ""}
     response = requests.post(url, data=data)
     return response.text
 
-def dataParser(raw_data):
-    data_list=[]
-    pop_data=json.loads(raw_data)
+
+def get_data():
+    url = "http://123.127.175.45:8082/ajax/GwtWaterHandler.ashx"
+    data = {"Method": "SelectRealData"}
+    response = requests.post(url, data=data)
+    return response.text
+
+
+def data_parser(raw_data):
+    data_list = []
+    pop_data = json.loads(raw_data)
     for pop_dict in pop_data:
-        siteNames=['北京','天津','河北']
+        siteNames = ['北京', '天津', '河北']
         if any(x in pop_dict['siteName'] for x in siteNames):
             data_list.append(pop_dict)
     return data_list
 
 
 if __name__ == '__main__':
-    raw_data=getData()
-    list=dataParser(raw_data)
-    for i in list:
-        print(i)
+    res = get_connect()
+    print(res)
+    # raw_data = get_data()
+    # print(raw_data)
+
+    # list = data_parser(raw_data)
+    # for i in list:
+    # print(i)
